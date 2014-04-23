fce67c12dfdb7e0115309ecc694fa132a16411b3
Aron Fyodor Asor <aronasorman@gmail.com>
2014-04-22 21:21:52 -0700

    skeleton files for a scrapy project

---
+++ sipper/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for a project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'sipper'
+
+SPIDER_MODULES = ['sipper.spiders']
+NEWSPIDER_MODULE = 'sipper.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'a (+http://www.yourdomain.com)'
